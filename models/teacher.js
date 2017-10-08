ec5bd807fdc1cb2b5fa55d6904e34b416fce11ee
lokiloko <ianw.tkj2@gmail.com>
2017-10-08 16:48:26 +0700

    add validation to teacher emails, and add confirm js when delete

--- models/teacher.js
+++ models/teacher.js
@@ -4,13 +4,38 @@ module.exports = (sequelize, DataTypes) => {
   var Teacher = sequelize.define('Teacher', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'email format is incorrect'
+        },
+        isUnique: function(value, next) {
+          Teacher.find({
+            where: {
+              email: value,
+              id: {
+                [sequelize.Op.not]: this.id
+              }
+            }
+          }).then(function(data) {
+            if (data) {
+              return next('Email Already Used')
+            }
+            return next();
+          }).catch(function(err) {
+            console.log(err);
+          })
+        }
+      }
+    },
     SubjectId: DataTypes.INTEGER
   });
-  Teacher.associate = function(models){
+  Teacher.associate = function(models) {
     models.Teacher.belongsTo(models.Subject);
   }
-  Teacher.prototype.fullname = function () {
+  Teacher.prototype.fullname = function() {
     return fullName(this.first_name, this.last_name);
   }
   return Teacher;
