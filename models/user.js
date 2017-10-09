40682d212ee9428280a8b30cab1e2d7081c65fb9
lokiloko <ianw.tkj2@gmail.com>
2017-10-09 20:20:22 +0700

    add required field on views, add uniques constraint on username and salt at table user

--- models/user.js
+++ models/user.js
@@ -3,26 +3,50 @@ const cryptoString = require('../helpers/cryptoString.js');
 const crypto = require('crypto');
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        isUnique: function(value, next) {
+          User.find({
+            where: {
+              username: value,
+              id: {
+                [sequelize.Op.not]: this.id
+              }
+            }
+          }).then(data => {
+            if (data) {
+              return next('Username already used');
+            }
+            return next();
+          }).catch((err) => {
+            console.log(err);
+          })
+        }
+      }
+    },
     password: DataTypes.STRING,
     role: DataTypes.STRING,
     salt: {
       type: DataTypes.STRING,
       validate: {
         notEmpty: {
-          args:true,
-          msg:'Salt must have value'
+          args: true,
+          msg: 'Salt must have value'
         },
         isUnique: function cek(value, next) {
           User.find({
-            where: {salt:value}
-          }).then(function(data){
-            if(data){
+            where: {
+              salt: value
+            }
+          }).then(data => {
+            if (data) {
               value = cryptoString();
               cek(value, next);
             }
+            this.salt = value;
             return next();
-          }).catch(function(err){
+          }).catch(function(err) {
             console.log(err);
           })
         }
@@ -35,14 +59,13 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.prototype.checkHashPassword = function(password) {
     var hash = crypto.createHmac('sha256', this.salt).update(password).digest('hex');
-    if(hash == this.password){
+    if (hash == this.password) {
       return true;
-    }
-    else{
+    } else {
       return false;
     }
   }
-  User.beforeCreate((user)=>{
+  User.beforeCreate((user) => {
     var hash = crypto.createHmac('sha256', user.salt).update(user.password).digest('hex');
     user.password = hash;
   })
